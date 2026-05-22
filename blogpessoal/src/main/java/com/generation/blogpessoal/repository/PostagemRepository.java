98a3a5a39e229cd641b83f151fc22fc18372095a
rntoscano <anny.toscano@hotmail.com.com>
2026-05-22 15:49:41 -0300

    Blog

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,13 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
 
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
